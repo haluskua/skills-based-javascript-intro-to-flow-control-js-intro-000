a68238f1f0ecd11e7d39e3e2da89d735b0edc57a
Omphalus Kua <haluskua.one@gmail.com>
2019-04-09 04:44:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,16 +7,23 @@ function teenager(age) {
   if (age >= 13 && age <= 19) {
   return "You are a teenager!";
   } else {
-    return "You are not a teenager";}
-}
-
-function ageChecker(age){
-  if (age >=13 && <= 19){
-    return 'You are a teenager!".'
-  } else if {
-      
+    return "You are not a teenager";
   }
 }
+
+
+function ageChecker(age) {
+  if (age >=13 && <= 19) {
+    return 'You are a teenager!';
+  } else if (age <=12) {
+      return 'You are a kid';
+    } else {
+        return 'You are a grownup';
+    }
+}
+
+
+
 function ternaryTeenager(age) {
 
 }
