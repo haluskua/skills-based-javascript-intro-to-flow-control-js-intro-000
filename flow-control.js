661c7357139de7835e385d77b2e552b4e63134d2
Omphalus Kua <haluskua.one@gmail.com>
2019-04-09 05:19:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,11 +25,8 @@ function ageChecker(age) {
 
 
 function ternaryTeenager(age) {
-  if (age >= 13 && age <= 19) ? "You are a teenager!" : "You are not a teenager!";
-}
-
-conditionToTest ? valueToBeReturnedIfTrue : valueToBeReturnedIfFalse
-
+"You are not a teenager!";
+}  if (age >= 13 && age <= 19) ? "You are a teenager!" : 
 
 
 
